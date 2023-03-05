0ae308755280e87b928e94c6e4ad071fe9774fa9
Erik Russell <erikrussells@gmail.com>
2023-03-05 16:29:36 -0500

    updated random

--- src/Random.js
+++ src/Random.js
@@ -2,7 +2,7 @@
 function Random() {
     return (
         <div className="random-body-div" >
-            <h2 className="random-text" >blah, blah, blah!</h2>
+            <h2 className="random-text" >Blah, Blah, Blah!</h2>
             
         </div>
     )
