c3061a6e1df0b9bed003de2a9c62588be02e6c18
Erik Russell <erikrussells@gmail.com>
2023-08-16 12:46:29 -0400

    playing with text hover to show an image

--- src/Landing.js
+++ src/Landing.js
@@ -5,30 +5,37 @@ import Extra from "./Extra";
 import Footer from "./Footer";
 // import Random from "./Random";
 import Card from "./Card";
-import ScrollButton from './ScrollButton';
+import ScrollButton from "./ScrollButton";
 import LandingClock from "./Landing-Clock";
 import Menu from "./Menu";
-import { ScrollToTop } from 'react-router-scroll-to-top';
-
+import { ScrollToTop } from "react-router-scroll-to-top";
 
 function Landing() {
-
   return (
     <div className="landing-div-body">
-
-
       {/* <Heading /> */}
       <div className="landing-text-div" id="under-line-div">
-      <LandingClock />
-        <h1 className="title" id="under-line">
+        <LandingClock />
+        <h1 contentEditable className="title" id="under-line">
           Erik Russell
         </h1>
 
-        <div className="else-link" >
-        <img className="bio-pic" src="image1.jpeg" alt=""></img>
-          <p className="undertext">Software Engineer</p>
+        <div className="else-link">
+          <img className="bio-pic" src="image1.jpeg" alt=""></img>
+          <p className="undertext">
+            <a className="hidden-a" href="/">
+              Software Engineer
+              <span className="hidden-span">
+                <img
+                  className="hidden-img"
+                  src="https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg"
+                  alt=""
+                  width="250"
+                />
+              </span>
+            </a>
+          </p>
         </div>
-        
       </div>
       <Card />
       {/* <WorkBody /> */}
@@ -38,7 +45,7 @@ function Landing() {
       <Extra />
       <Footer />
       <ScrollButton />
-      < ScrollToTop />
+      <ScrollToTop />
     </div>
   );
 }
